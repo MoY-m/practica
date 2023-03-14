66d2ffacd6bb2ea14f1525b5af46bd917ed32283
MoY-m <107066626+MoY-m@users.noreply.github.com>
2023-03-14 13:35:19 -0600

    add index
    archivo index

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type= "text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="js/jquery-3.6.4.min.js">
+    <title>practica 1 | include con php</title>
+</head>
+<body>
+    <!--navbar -->
+<?php include "include/navbar.php"; ?>
+    <!--termina navbar-->
+    <script scr="js/bootstrap.min.js"></script>
+</body>
+</html>
